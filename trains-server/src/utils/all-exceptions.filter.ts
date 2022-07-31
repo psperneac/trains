b7d5ac82d8a7b5bf159a084fce89d5fe41d2e631
Paul Sperneac <gepetto@gmail.com>
2022-07-31 00:45:52 -0400

    fixed global exceptions filter 2

--- trains-server/src/utils/all-exceptions.filter.ts
+++ trains-server/src/utils/all-exceptions.filter.ts
@@ -4,7 +4,7 @@ import {
   ExceptionFilter,
   HttpStatus,
   HttpException,
-  Logger,
+  Logger, UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { QueryFailedError, EntityNotFoundError, CannotCreateEntityIdMapError } from 'typeorm';
@@ -47,79 +47,49 @@ export class AllExceptionsFilter implements ExceptionFilter {
     this.logger = new Logger();
   }
 
-  catch(exception: Error, host: ArgumentsHost): any {
+  catch(ex: Error, host: ArgumentsHost): any {
     const ctx = host.switchToHttp();
     const request = ctx.getRequest();
     const response = ctx.getResponse();
-    let message = (exception as any)?.message?.message;
+    let message = (ex as any)?.message?.message;
     let code = 'HttpException';
 
-    Logger.error(process.env, message, (exception as any)?.stack, `${request.method} ${request.url}`);
+    Logger.error(process.env, message, (ex as any)?.stack, `${request.method} ${request.url}`);
 
     let status;
 
-    switch (exception.constructor) {
+    switch (ex.constructor) {
       case HttpException:
-        status = (exception as HttpException).getStatus();
+        status = (ex as HttpException).getStatus();
         break;
       case QueryFailedError:  // this is a TypeOrm error
         status = HttpStatus.UNPROCESSABLE_ENTITY
-        message = (exception as QueryFailedError).message;
-        code = (exception as any).code;
+        message = (ex as QueryFailedError).message;
+        code = (ex as any).code;
         break;
       case EntityNotFoundError:  // this is another TypeOrm error
         status = HttpStatus.UNPROCESSABLE_ENTITY
-        message = (exception as EntityNotFoundError).message;
-        code = (exception as any).code;
+        message = (ex as EntityNotFoundError).message;
+        code = (ex as any).code;
         break;
       case CannotCreateEntityIdMapError: // and another
         status = HttpStatus.UNPROCESSABLE_ENTITY
-        message = (exception as CannotCreateEntityIdMapError).message;
-        code = (exception as any).code;
+        message = (ex as CannotCreateEntityIdMapError).message;
+        code = (ex as any).code;
+        break;
+      case UnauthorizedException:
+        status = HttpStatus.UNAUTHORIZED;
+        message = (ex as UnauthorizedException).message;
+        code = (ex as any).code;
         break;
       default:
         status = HttpStatus.INTERNAL_SERVER_ERROR
-        code = exception.constructor.toString();
+        code = ex.constructor.toString();
         break;
     }
 
     response.status(status).json(GlobalResponseError(status, message, code, request,
-      (exception as any)?.stack.split('\n').map(str => str.trim())));
-
-    // }
-    //
-    //   const statusCode =
-    //     exception instanceof HttpException
-    //       ? exception.getStatus()
-    //       : HttpStatus.INTERNAL_SERVER_ERROR;
-    //   const message =
-    //     exception instanceof HttpException
-    //       ? exception.message
-    //       : 'Internal server error';
-    //
-    //   const devErrorResponse: any = {
-    //     statusCode,
-    //     timestamp: new Date().toISOString(),
-    //     path: request.url,
-    //     method: request.method,
-    //     errorName: exception?.name,
-    //     message: exception?.message,
-    //   };
-    //
-    //   const prodErrorResponse: any = {
-    //     statusCode,
-    //     message,
-    //   };
-    //
-    //   this.logger.log(`request method: ${request.method} request url${request.url}`, JSON.stringify(devErrorResponse));
-    //
-    //   response
-    //     .status(statusCode)
-    //     .json(
-    //       process.env.NODE_ENV === 'development'
-    //         ? devErrorResponse
-    //         : prodErrorResponse,
-    //     );
+      (ex as any)?.stack.split('\n').map(str => str.trim())));
   }
 }
 
