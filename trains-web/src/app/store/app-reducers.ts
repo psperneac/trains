6098a3ef713c7925ce9a59b4c9974893c2c4838a
Paul Sperneac <gepetto@gmail.com>
2023-02-19 23:57:47 -0500

    updated ngrx@15

--- trains-web/src/app/store/app-reducers.ts
+++ trains-web/src/app/store/app-reducers.ts
@@ -1,4 +1,4 @@
-import {getSelectors, routerReducer} from '@ngrx/router-store';
+import {getRouterSelectors, routerReducer} from '@ngrx/router-store';
 import {createSelector} from '@ngrx/store';
 
 export interface AppState {
@@ -16,5 +16,5 @@ export const reducers = {
 export const metaReducers = [];
 
 // router selectors
-export const routerSelectors = getSelectors((state: AppState) => state.router);
+export const routerSelectors = getRouterSelectors((state: AppState) => state.router);
 export const routerSelectReturnUrl = createSelector(routerSelectors.selectQueryParam('returnUrl'), param => param);
