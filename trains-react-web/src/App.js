0edb9d155456dc9e3a6000cbbc312fb604c8fcae
Paul Sperneac <gepetto@gmail.com>
2023-03-08 20:37:25 -0500

    work on react user website

--- trains-react-web/src/App.js
+++ trains-react-web/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
+import { Icon } from "leaflet";
+import parkData from "./data/skateboard-parks.js";
+import { Counter } from './features/counter/Counter';
 
 function App() {
+  const [activePark, setActivePark] = useState(null);
+  console.log(activePark);
+  const icon = new Icon({
+    iconUrl: "/skateboarding.svg",
+    iconSize: [25, 25]
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <><MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={true}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+      {parkData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={[
+            park.geometry.coordinates[1],
+            park.geometry.coordinates[0]
+          ]}
+          eventHandlers={{
+            click: () => {
+              console.log(park);
+              setActivePark(park);
+            }
+          }}
+          icon={icon} />
+      ))}
+
+      {activePark && (
+        <Popup
+          position={[
+            activePark.geometry.coordinates[1],
+            activePark.geometry.coordinates[0]
+          ]}
+          onClose={() => {
+            setActivePark(null);
+          } }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            <h2>{activePark.properties.NAME}</h2>
+            <p>{activePark.properties.DESCRIPTIO}</p>
+          </div>
+        </Popup>
+      )}
+    </MapContainer><Counter></Counter></>
   );
 }
 
